eb4194ba9d92909b8146a1cbe779e6303e1c7b3a
Pedro Henrique <Pedro Henrique@PC-PEDRO>
2017-02-03 23:24:15 -0200

    Alterações para o selenium web driver.

--- src/ChromeBrowser/LaunchApplication.java
+++ src/ChromeBrowser/LaunchApplication.java
@@ -2,6 +2,9 @@ package ChromeBrowser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,17 +15,55 @@ public class LaunchApplication {
 	public static void main(String[] args) 
 	{
 	
-		System.setProperty("webdriver.chrome.driver", "C:\\Desenvolvimento\\Selenium\\DriverChrome\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\DriverChrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();	
 		
-		driver.get("https://mobileid.certisign.com.br/banking/");
+		//driver.get("https://srv2012r2-19/CertiMedManager/gestor/public/cadastro#/novo-pro-hsm");
+		driver.get("https://srv2012r2-19/CertiMedManager/gestor/public/cadastro#/novo-vault");
 		
 		GeraCPF CPF = new GeraCPF();
 		
-		WebElement campoDeTexto = driver.findElement(By.id("txtCPF"));
-		campoDeTexto.sendKeys(CPF.generate());
+		WebElement cpf = driver.findElement(By.id("cpf"));
+		cpf.sendKeys(CPF.generate());
 		
-		WebElement botaoAutenticar = driver.findElement(By.id("btnAutenticar"));
+		// Inicializar nome
+		WebElement nameVazio = driver.findElement(By.id("nome"));
+		nameVazio.sendKeys("");
+		
+		WebElement nameVazio2 = driver.findElement(By.id("nome"));
+		nameVazio2.sendKeys("");
+		
+		WebElement name = driver.findElement(By.id("nome"));
+		name.sendKeys("PEDRO HENRIQUE");
+		
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("pedro.inacio@teste.com.br");
+		
+		WebElement dataNascimento = driver.findElement(By.id("dataNascimento"));
+		dataNascimento.sendKeys("11111991");
+		
+		WebElement rg = driver.findElement(By.id("rg"));
+		rg.sendKeys("481144663");
+		
+		WebElement rgOrgaoExp = driver.findElement(By.id("rgOrgaoExpedidor"));
+		rgOrgaoExp.sendKeys("SSP");
+		
+		WebElement rgUf = driver.findElement(By.id("rgUf"));
+		rgUf.sendKeys("SP");
+		
+		WebElement telefone = driver.findElement(By.id("telefone"));
+		telefone.sendKeys("1145012442");
+		
+		WebElement especialidade = driver.findElement(By.id("especialidade"));
+		especialidade.sendKeys("CLINICO GERAL");
+		
+		WebElement senha = driver.findElement(By.id("senha"));
+		senha.sendKeys("1234");
+		
+		WebElement confirmacao = driver.findElement(By.id("confirmacao"));
+		confirmacao.sendKeys("1234");
+		
+		WebElement botaoAutenticar = driver.findElement(By.className("btn-solicitar-certificado"));
 		botaoAutenticar.click();
 		
 	}
--- src/FirefoxBrowser/LaunchApplicationFirefox.java
+++ src/FirefoxBrowser/LaunchApplicationFirefox.java
@@ -11,25 +11,54 @@ public class LaunchApplicationFirefox {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.gecko.driver", "C:\\Desenvolvimento\\Selenium\\DriverFirefox\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\DriverFirefox\\geckodriver.exe");
 		
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://mobileid.certisign.com.br/banking/");
-		driver.quit();
 		
+		driver.get("https://srv2012r2-19/CertiMedManager/gestor/public/cadastro#/novo-pro-hsm");
 		
 		GeraCPF CPF = new GeraCPF();
 		
-		WebElement campoDeTexto = driver.findElement(By.id("txtCPF"));
-		campoDeTexto.sendKeys(CPF.generate());
+		WebElement cpf = driver.findElement(By.id("cpf"));
+		cpf.sendKeys(CPF.generate());
 		
-		//WebElement botaoAutenticar = driver.findElement(By.id("btnAutenticar"));
-		//botaoAutenticar.click();
+		// Inicializar nome
+		WebElement nameVazio = driver.findElement(By.id("nome"));
+		nameVazio.sendKeys("");
+		
+		WebElement name = driver.findElement(By.id("nome"));
+		name.sendKeys("PEDRO HENRIQUE FRANCISCO INACIO");
+		
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("pedro.inacio@teste.com.br");
+		
+		WebElement dataNascimento = driver.findElement(By.id("dataNascimento"));
+		dataNascimento.sendKeys("11111991");
+		
+		WebElement rg = driver.findElement(By.id("rg"));
+		rg.sendKeys("481144663");
+		
+		WebElement rgOrgaoExp = driver.findElement(By.id("rgOrgaoExpedidor"));
+		rgOrgaoExp.sendKeys("SSP");
+		
+		WebElement rgUf = driver.findElement(By.id("rgUf"));
+		rgUf.sendKeys("SP");
+		
+		WebElement telefone = driver.findElement(By.id("telefone"));
+		telefone.sendKeys("1145012442");
+		
+		WebElement especialidade = driver.findElement(By.id("especialidade"));
+		especialidade.sendKeys("CLINICO GERAL");
+		
+		WebElement senha = driver.findElement(By.id("senha"));
+		senha.sendKeys("1234");
+		
+		WebElement confirmacao = driver.findElement(By.id("confirmacao"));
+		confirmacao.sendKeys("1234");
+		
+		WebElement botaoAutenticar = driver.findElement(By.className("btn-solicitar-certificado"));
+		botaoAutenticar.click();
 		
 	}	
-	
-
-		
-	
 	
 }
